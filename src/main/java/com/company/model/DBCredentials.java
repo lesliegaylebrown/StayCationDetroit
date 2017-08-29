02cb1f7add467326ac44e3babef9021bb85da097
Jonathon Branham <kefele@aim.com>
2017-08-29 08:27:06 -0500

    pull attempt

--- src/main/java/com/company/model/DBCredentials.java
+++ src/main/java/com/company/model/DBCredentials.java
@@ -3,7 +3,7 @@ package com.company.model;
 //this file would be listed in the .gitignore
 //so that this info remains confidential
 public class DBCredentials {
-    public static final String DB_ADDRESS = "jdbc:mysql://localhost:3306/staycation";
+    public static final String DB_ADDRESS = "jdbc:mysql://staycation-mysql.cvjxsmoxxjoi.us-east-2.rds.amazonaws.com:3306/staycation";
     public static final String USERNAME = "awsuser";
     public static final String PASSWORD = "YouCanNotComeIn";
 }
